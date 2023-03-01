20639ba1253699101d42482405c14904a690fd30
adixks <adrianks@op.pl>
2023-03-01 11:46:47 +0100

    Added dependencies and properties.

---
+++ src/main/java/pl/ksiezak/adrian/java/googleApi/GoogleApiApplication.java
@@ -0,0 +1,12 @@
+package pl.ksiezak.adrian.java.googleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleApiApplication.class, args);
+	}
+}
